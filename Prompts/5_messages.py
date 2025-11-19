5eea7d40cbbe5b3b7495d1d039b30cddb2aa6382
Somil Dogra <151395938+somil-18@users.noreply.github.com>
2025-11-19 18:28:07 +0530

    Add files via upload

---
+++ Prompts/5_messages.py
@@ -0,0 +1,29 @@
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="conversational",
+    max_new_tokens=150,
+    temperature=0.3,
+    huggingfacehub_api_token=TOKEN
+)
+
+chat = ChatHuggingFace(llm=llm)
+
+chat_history = [
+    SystemMessage(content="You are a helpful assistant"),
+    HumanMessage(content="Tell me about Langchain")
+]
+
+result = chat.invoke(chat_history)
+
+chat_history.append(AIMessage(content=result.content))
+
+print(chat_history)
