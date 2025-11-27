b06dc98675d05ae40281be93172177b5058a9ccd
Somil Dogra <151395938+somil-18@users.noreply.github.com>
2025-11-27 18:35:40 +0530

    Add files via upload

---
+++ TextSplitters/2_cb_dl.py
@@ -0,0 +1,20 @@
+from langchain_classic.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+
+loader = PyPDFLoader(r'DocumentLoader/dl.pdf')
+
+docs = loader.load()
+
+splitter = CharacterTextSplitter(
+    chunk_size = 100,
+    chunk_overlap = 0,
+    separator = ' '
+)
+
+result = splitter.split_documents(docs)
+
+print(result[0])
+print('\n')
+
+print(result[0].page_content)
+
